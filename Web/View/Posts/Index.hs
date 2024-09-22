fcb5e25169d3796a3866e0083848b02f924af02d
JosephMaltese <149339371+JosephMaltese@users.noreply.github.com>
2024-09-22 04:14:03 +0000

    Implemented home page UI

--- Web/View/Posts/Index.hs
+++ Web/View/Posts/Index.hs
@@ -5,25 +5,22 @@ data IndexView = IndexView { posts :: [Post] }
 
 instance View IndexView where
     html IndexView { .. } = [hsx|
+        <head>
+            <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
+        </head>
+        <body>
         <div style="display: flex; flex-direction: row; justify-content: space-between;">
             {breadcrumb}
             <a class="js-delete js-delete-no-confirm" href={DeleteSessionAction}>Logout</a>
         </div>
-        <h1>Home<a href={pathTo NewPostAction} class="btn btn-primary ms-4">+ New</a></h1>
-        <div class="table-responsive">
-            <table class="table">
-                <thead>
-                    <tr>
-                        <th>Post</th>
-                        <th></th>
-                        <th></th>
-                    </tr>
-                </thead>
-                <tbody>{forEach posts renderPost}</tbody>
-            </table>
-            
+        <div style="display: flex; justify-content: center;">
+            <h1 style="font-size: 5rem;">STaBLit</h1>
         </div>
-    |]
+        <h1>Home<a href={pathTo NewPostAction} class="btn btn-primary ms-4">+ New Post</a></h1>
+        <hr>
+        <div>{forEach posts renderPost}</div>
+        </body>
+        |]
         where
             breadcrumb = renderBreadcrumb
                 [ breadcrumbLink "Posts" PostsAction
@@ -31,12 +28,31 @@ instance View IndexView where
 
 renderPost :: Post -> Html
 renderPost post = [hsx|
-    <tr>
-        <td><a href={ShowPostAction post.id}>{post.title}</a></td>
-        <td><p>Author: {post.author}</p></td>
+    <div>
+            <div style="display: flex; flex-direction: row; justify-content: space-between;">
+                <p>Author: {post.author}</p>
+                {renderEditDeleteButtons post}
+            </div>
+            <a href={ShowPostAction post.id} style="font-size: 1.5rem;">{post.title}</a>
+            <p style="margin-top: 1rem;">{post.body}</p>
 
-        {renderEditDeleteButtons post}
-    </tr>
+            <div style="display: flex; flex-direction: row; align-items: center;">
+
+                <p style="margin-top: 1rem;">0</p>
+                <button class="btn btn-like">
+                    <i class="fas fa-thumbs-up"></i> Like
+                </button>
+
+                <p style="margin-top: 1rem;">0</p>
+                <button class="btn btn-dislike">
+                    <i class="fas fa-thumbs-down"></i> Dislike
+                </button>
+
+                <a href={NewCommentAction post.id} style="align-items: center;">
+                    <p style="margin-top: 1rem;">Comment</p>
+                </a>
+            </div>
+    </div>
 |]
 
 
@@ -44,7 +60,9 @@ renderEditDeleteButtons :: Post -> Html
 renderEditDeleteButtons post =
     if currentUser.email == post.author
     then [hsx|
-        <td><a href={EditPostAction post.id} class="text-muted">Edit</a></td>
-        <td><a href={DeletePostAction post.id} class="js-delete text-muted">Delete</a></td>
+        <div style="display: flex; flex-direction: row;">
+            <a href={EditPostAction post.id} class="text-muted">Edit</a>
+            <a href={DeletePostAction post.id} class="js-delete text-muted" style="margin-left: 2rem;">Delete</a>
+        </div>
     |]
     else mempty
